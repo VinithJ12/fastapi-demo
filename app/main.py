9c32e7f3a754d3e4b3d9a1258fa5a1520acdfa16
root <root@Vinith>
2024-10-28 19:30:07 -0400

    added endpoint

--- app/main.py
+++ app/main.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
+from typing import Optional 
+from pydantic import BaseModel # allows us to model data
 import json
 import os
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
+@app.get("/")  # zone apex z3 #decorator
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Hello": "Hello Vinith, How's your day?"} # returns json
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+@app.get("/square/{a}")
+def square(a: int):
+    return {"square": a*a}
+
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+   return {"product": c * d}
+
+@app.get("/subtract/{e}/{f}")
+def multiply(e: int, f: int):
+   return {"subtract": e - f}
