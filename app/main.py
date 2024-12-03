b37f418b75f80d4169eeba4af55727709e16485c
root <root@Vinith>
2024-12-03 00:24:34 -0500

    Data Project Fast API

--- app/main.py
+++ app/main.py
@@ -1,25 +1,65 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from typing import Optional 
-from pydantic import BaseModel # allows us to model data
+import mysql.connector
+from mysql.connector import Error
 import json
 import os
+#import pandas as pd
+
+DBHOST= "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER= "ds2022"
+DBPASS=os.getenv('DBPASS')
+DB="uhe5bj"
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")  # zone apex z3 #decorator
-def zone_apex():
-    return {"Hello": "Hello Vinith, How's your day?"} # returns json
 
-@app.get("/square/{a}")
-def square(a: int):
-    return {"square": a*a}
+@app.get('/genres')
+def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()	
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return {"Error": "MySQL Error: " + str(e)}
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-   return {"product": c * d}
-
-@app.get("/subtract/{e}/{f}")
-def multiply(e: int, f: int):
-   return {"subtract": e - f}
+@app.get ('/songs')
+def get_songs():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()    	
+    query= "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    try:
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        cur.close()
+        db.close()
+        return None
